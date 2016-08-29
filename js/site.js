6b2fde8a017a1752da81ceae1e49f284b18c0c65
Randy Keyes <imrandyk@gmail.com>
2016-08-29 08:42:50 -0500

    Fixed color string

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-var colors = ["#3498db'", "#1abc9c", "#f3a801"];
+var colors = ["#3498db", "#1abc9c", "#f3a801"];
 
 $(document).ready(function()//When the dom is ready or just add it if you already have a .ready function
 {
